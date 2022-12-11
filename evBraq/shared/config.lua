f4deaffd38d37e1ea704737df684af35bef93de1
Evann Dv <111243677+EvannDV@users.noreply.github.com>
2022-12-11 22:07:07 +0100

    Add files via upload

---
+++ evBraq/shared/config.lua
@@ -0,0 +1,26 @@
+--------------------- by evann™ ---------------------------
+Config = {}
+evMenu = {}
+--------------------- by evann™ ---------------------------
+
+
+
+
+evMenu = {  --> Position des braquages Ajoutez en autant que vous voulez
+    {pos = vector3(-1338.0723876953,119.10617828369,56.462963104248)};
+    {pos = vector3(-1335.1640625,116.8994140625,56.514381408691)};
+}
+
+
+Config = {
+    Blips = true,    --> Si vous voulez les blips des différents braquages dispo
+    Time = 5000,  --> Temps en ms avant de recevoir l'argent
+    Tempo = 10000,  --> Temps entre deux braquages possibles
+    Money = 700,   --> Reward du Braquage
+}
+
+
+
+
+
+
